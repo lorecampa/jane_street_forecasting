aeec6c078e73c4fe87a061309e907a3b9ef8e1eb
lorecampa <lorenzo.campana@mail.polimi.it>
2024-11-15 15:51:34 +0100

    init

---
+++ prj/config.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+SRC_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = SRC_DIR.parent
+DATA_DIR = ROOT_DIR / 'dataset'
+EXP_DIR = ROOT_DIR / 'experiments'
+KAGGLE_EVAL_DIR = ROOT_DIR / 'kaggle_evaluation'
